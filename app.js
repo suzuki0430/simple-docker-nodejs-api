bf299cdee83cc8451e853ab372596f21a9ae0f79
Atsushi <shimarisu.araiguma@gmail.com>
2021-03-27 09:46:29 +0900

    docker

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var mongoose = require("mongoose");
-var apiController = require("./controllers/apiController");
+var mongoose = require('mongoose');
+var apiController = require('./controllers/apiController');
 
 var port = 3000;
 
-app.use("/assets", express.static(__dirname + "/public"));
+app.use('/assets', express.static(__dirname + '/public'));
 //mongo instead of localhost for docker-compose
 mongoose.connect(
-  "mongodb://localhost:27017/users",
+  'mongodb://mongo:27017/users',
   { useNewUrlParser: true, useUnifiedTopology: true },
-  err => {
+  (err) => {
     console.log(err);
   }
 );
